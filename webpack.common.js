88b9e4d9ce23fa163f58241e3d6f8582a733396c
MatMercer <contact@matbm.net>
2021-02-20 23:41:03 -0300

    Make all dist js files go to the /js path

--- webpack.common.js
+++ webpack.common.js
@@ -41,7 +41,7 @@ module.exports = {
     ],
   },
   output: {
-    filename: '[name].[contenthash].js',
+    filename: 'js/[name].[contenthash].js',
     path: path.resolve(__dirname, 'dist'),
   },
   optimization: {
